6c9a601e21eb04b3d5ce5fbc3269423da8023228
chrissherfield <christopher.sherfield@gmail.com>
2018-04-17 19:17:29 +0100

    Added a complex class. Currently contains nothing. Was used to test Unit Tests using CUTE

---
+++ Complex.cpp
@@ -0,0 +1,21 @@
+/*
+ * Complex.cpp
+ *
+ *  Created on: 11 Apr 2018
+ *      Author: chrissherfield
+ */
+
+#include "Complex.h"
+
+namespace cstools {
+
+Complex::Complex() : temp(0) {
+	// TODO Auto-generated constructor stub
+
+}
+
+Complex::~Complex() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace cstools */
---
+++ Complex.h
@@ -0,0 +1,23 @@
+/*
+ * Complex.h
+ *
+ *  Created on: 11 Apr 2018
+ *      Author: chrissherfield
+ */
+
+#ifndef COMPLEX_H_
+#define COMPLEX_H_
+
+namespace cstools {
+
+class Complex {
+public:
+	Complex();
+	virtual ~Complex();
+
+	int temp;
+};
+
+} /* namespace cstools */
+
+#endif /* COMPLEX_H_ */
